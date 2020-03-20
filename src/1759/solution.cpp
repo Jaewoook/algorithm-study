1e2e902f0b9b1e1da2a706a4d6e52a1809d67e69
Jaewook Ahn <ajw4586@gmail.com>
2020-03-20 20:55:27 +0900

    Add: 1759 solution

--- src/1759/solution.cpp
+++ src/1759/solution.cpp
@@ -1,61 +1,62 @@
-// 나중에 풀 문제
 #include <iostream>
 #include <string>
 #include <vector>
 #include <algorithm>
-
 using namespace std;
 
-void combination(string* result[], int n) {
-    for(int i = 0; i < )
+const char VOWELS[] = {'a', 'e', 'i', 'o', 'u'};
+
+void dfs(char letter, vector<char> &letters, vector<char> &pw, int L, int consonant_count, int vowel_count) {
+    vector<char>::iterator iter = find(letters.begin(), letters.end(), letter);
+    if (L == pw.size()) {
+        if (consonant_count >= 2 && vowel_count >= 1) {
+            for (int i = 0; i < L; i++) {
+                cout << pw[i];
+            }
+            cout << "\n";
+        }
+    } else {
+        for (; iter != letters.end(); iter++) {
+            pw.push_back(*iter);
+            bool is_vowel = false;
+            for (int i = 0; i < 5; i++) {
+                if (VOWELS[i] == *iter) {
+                    is_vowel = true;
+                    break;
+                }
+            }
+            if (is_vowel) {
+                vowel_count++;
+            } else {
+                consonant_count++;
+            }
+
+            dfs(*(iter + 1), letters, pw, L, consonant_count, vowel_count);
+            pw.pop_back();
+            if (is_vowel) {
+                vowel_count--;
+            } else {
+                consonant_count--;
+            }
+        }
+    }
 }
 
 int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
     int L = 0, C = 0;
-    string vowel[] = {"a", "e", "i", "o", "u"};
+    int vowels = 0, consonants = 0;
     cin >> L >> C;
-    string letter;
-    string result[C];
-    vector<string> vowels, consonants;
-    vowels.reserve(30);
-    consonants.reserve(30);
-
-    for(int i = 0; i < C; i++) {
-        cin >> letter;
-        bool is_vowel = false;
-        for (int j = 0; j < 5; j++) {
-            if(letter.find(vowel[j]) != string::npos) {
-                vowels.push_back(letter);
-                is_vowel = true;
-            }
-        }
-        if (!is_vowel) {
-            consonants.push_back(letter);
-        }
+    vector<char> letters(C), password;
+    for (int i = 0; i < C; i++) {
+        cin >> letters[i];
     }
+    sort(letters.begin(), letters.end());
 
-    for (int i = max((int) (L - vowels.size()), 0); i < max(L, (int) vowels.size()); i++) {
-        for (int j = 0; j <= i; j++) {
-            result[j] = vowels[j]
-        }
-
-        for (int j = C - i - 1; j < C; j++) {
-
-        }
-    }
-
-    // debug
-    cout << "자음: ";
-    for(int i = 0; i < consonants.size(); i++) {
-        cout << consonants[i] << " ";
-    }
-    cout << "\n모음: ";
-
-    for(int i = 0; i < vowels.size(); i++) {
-        cout << vowels[i] << " ";
-    }
-    cout << "\n";
-
+    dfs(letters[0], letters, password, L, 0, 0);
 
     return 0;
 }
