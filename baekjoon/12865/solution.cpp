cdb1407eefc553ab0ff8f7701a3ff5727cc745ab
Jaewook Ahn <ajw4586@gmail.com>
2020-04-22 23:39:12 +0900

    Add: 12865 solution

--- baekjoon/12865/solution.cpp
+++ baekjoon/12865/solution.cpp
@@ -1,39 +1,50 @@
-//  TODO
 #include <iostream>
 #include <algorithm>
 #include <vector>
 #include <utility>
 using namespace std;
 
-int max(int a, int b) {
-    return a > b ? a : b;
-}
-
 int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
     int N = 0, K = 0, W = 0, V = 0;
-    vector<pair<int, int> > o;
 
     cin >> N >> K;
-    // pair<int, int> *dp = new pair<int, int>[N];
-    pair<int, int> *arr = new pair<int, int>[N];
-    int *dp = new int[K + 1];
-    fill_n(dp, K + 1, 0);
+    pair<int, int> *wv = new pair<int, int>[N];
+    int **dp = new int*[N];
+    for (int i = 0; i < N; i++) {
+        dp[i] = new int[K + 1];
+        fill_n(dp[i], K + 1, 0);
+    }
 
     for (int i = 0; i < N; i++) {
         cin >> W >> V;
-
-        arr[i] = make_pair(W, V);
+        wv[i] = make_pair(W, V);
     }
 
-    for (int i = 1; i <= K; i++) {
-        for (int j = 0; j < N; j++) {
-            arr[j].first
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j <= K; j++) {
+            if (i == 0) {
+                if (wv[i].first <= j) {
+                    dp[i][j] = wv[i].second;
+                }
+                continue;
+            }
+
+            if (wv[i].first <= j) {
+                dp[i][j] = max(dp[i - 1][j], wv[i].second + dp[i - 1][j - wv[i].first]);
+            } else {
+                dp[i][j] = dp[i - 1][j];
+            }
         }
     }
 
-    cout << max_value << "\n";
+    cout << dp[N - 1][K] << "\n";
 
+    for (int i = 0; i < N; i++) {
+        delete[] dp[i];
+    }
+    delete[] wv;
     delete[] dp;
-    delete[] arr;
     return 0;
 }
