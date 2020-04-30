6c02224a05b34e0f038fca482a9709b5048ccd4a
Jaewook Ahn <ajw4586@gmail.com>
2020-05-01 01:12:32 +0900

    Add: 1103 solution

--- baekjoon/1103/solution.cpp
+++ baekjoon/1103/solution.cpp
@@ -1,98 +1,39 @@
 #include <iostream>
-#include <queue>
+#include <algorithm>
+#include <cmath>
 #include <utility>
 using namespace std;
 
-const int d[4][2] = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
+const int d[4][2] = {{-1, 0}, {0, -1}, {0, 1}, {1, 0}};
 
-void print_board(char **board, int N, int M) {
-    cout << "debug board\n";
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < M; j++) {
-            cout << board[i][j] << " ";
-        }
-        cout << "\n";
+int dfs(int start_i, int start_j, int **dp, bool **v, char **board, int N, int M) {
+    if (start_i < 0 || start_i >= N || start_j < 0 || start_j >= M || board[start_i][start_j] == 'H') {
+        return 0;
     }
-    cout << "\n";
-}
 
-void print_dp(int **dp, int N, int M) {
-    cout << "\ndebug dp\n";
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < M; j++) {
-            cout << dp[i][j] << " ";
-        }
-        cout << "\n";
-    }
-    cout << "\n";
-}
-
-pair<int, int> convert_val_to_pos(int val, int N, int M) {
-    return make_pair(val / N, val % M);
-}
-
-int convert_pos_to_val(pair<int, int> pos, int M) {
-    return pos.first * M + pos.second;
-}
-
-pair<int, int> find_root(int i, int j, int **dp, int N, int M) {
-    int val = convert_pos_to_val(make_pair(i, j), M);
-
-    if (val != dp[i][j]) {
-        pair<int, int> root = convert_val_to_pos(dp[i][j], N, M);
-        pair<int, int> new_pos = find_root(root.first, root.second, dp, N, M);
-        dp[i][j] = dp[new_pos.first][new_pos.second];
-        return new_pos;
-    }
-    return make_pair(i, j);
-}
-
-int bfs(int **dp, char **board, int N, int M) {
-    queue<pair<int, int> > q;
-    pair<int, int> top;
     int next_i = 0, next_j = 0;
-    pair<int, int> root_pos;
-    int root_val = 0;
-    bool should_count_up = false;
-    int count = 0;
-
-    q.push(make_pair(0, 0));
-    while (!q.empty()) {
-        should_count_up = false;
-        top = q.front();
-        cout << "\nbfs: " << top.first << " " << top.second << "\n";
-        root_pos = find_root(top.first, top.second, dp, N, M);
-        root_val = convert_pos_to_val(root_pos, M);
-        // cout << "root: " << root_pos.first << " " << root_pos.second << " " << root_val << "\n";
-        q.pop();
-
-        for (int i = 0; i < 4; i++) {
-            next_i = top.first + d[i][0] * (board[top.first][top.second] - 48);
-            next_j = top.second + d[i][1] * (board[top.first][top.second] - 48);
-            // cout << "next: " << next_i << " " << next_j << "\n";
-
-            if (next_i < 0 || next_i >= N || next_j < 0 || next_j >= M || board[next_i][next_j] == 'H') {
-                continue;
-            }
-            cout << "next: " << next_i << " " << next_j << " board: " << (int) (board[next_i][next_j]) - 48 << "\n";
-
-            pair<int, int> next_root_pos = find_root(next_i, next_j, dp, N, M);
-            if (root_val == convert_pos_to_val(next_root_pos, M)) {
-                return -1;
-            } else {
-                should_count_up = true;
-                dp[next_i][next_j] = 0;
-                q.push(next_root_pos);
-            }
-        }
-
-        if (should_count_up) {
-            count++;
-            cout << "count up!\n";
-        }
+    if (v[start_i][start_j]) {
+        return -1;
     }
 
-    return count;
+    if (dp[start_i][start_j] > 0) {
+        return dp[start_i][start_j];
+    }
+
+    v[start_i][start_j] = true;
+    for (int i = 0; i < 4; i++) {
+        next_i = start_i + d[i][0] * (board[start_i][start_j] - 48);
+        next_j = start_j + d[i][1] * (board[start_i][start_j] - 48);
+
+        int result = dfs(next_i, next_j, dp, v, board, N, M);
+        if (result == -1) {
+            return -1;
+        }
+        dp[start_i][start_j] = max(dp[start_i][start_j], result + 1);
+    }
+
+    v[start_i][start_j] = false;
+    return dp[start_i][start_j];
 }
 
 int main() {
@@ -101,26 +42,23 @@ int main() {
 
     int N = 0, M = 0;
     cin >> N >> M;
+
     char **board = new char*[N];
     int **dp = new int*[N];
+    bool **v = new bool*[N];
     for (int i = 0; i < N; i++) {
         board[i] = new char[M];
         dp[i] = new int[M];
-
-        //  initialize dp array
-        for (int j = 0; j < M; j++) {
-            dp[i][j] = i * M + j;
-        }
+        v[i] = new bool[M];
+        fill_n(dp[i], M, 0);
+        fill_n(v[i], M, false);
     }
 
     for (int i = 0; i < N; i++) {
         cin >> board[i];
     }
 
-    print_dp(dp, N, M);
-    print_board(board, N, M);
-    cout << "count: " << bfs(dp, board, N, M) << "\n";
-    print_dp(dp, N, M);
+    cout << dfs(0, 0, dp, v, board, N, M) << "\n";
 
     for (int i = 0; i < N; i++) {
         delete[] board[i];
@@ -128,5 +66,6 @@ int main() {
     }
     delete[] board;
     delete[] dp;
+    delete[] v;
     return 0;
 }
