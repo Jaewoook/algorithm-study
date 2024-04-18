ebc57bd6e19781343c175599467c3bbcc3a6ee46
Jaewook Ahn <dev.jaewook@gmail.com>
2024-04-18 15:25:26 +0900

    solve(programmers): 더 맵게 using Python3
    - priority queue / heap algorithm

---
+++ programmers/lv2/더 맵게/solution.py
@@ -0,0 +1,16 @@
+# Lv. 2 - 더 맵게
+# https://school.programmers.co.kr/learn/courses/30/lessons/42626
+
+import heapq
+
+def solution(scoville, K):
+    answer = 0
+    heapq.heapify(scoville)
+
+    while scoville[0] < K and len(scoville) > 1:
+        min1 = heapq.heappop(scoville)
+        min2 = heapq.heappop(scoville)
+        heapq.heappush(scoville, min1 + min2 * 2)
+        answer += 1
+
+    return answer if scoville[0] >= K else -1
