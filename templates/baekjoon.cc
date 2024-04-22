4b0fe0dc895ef80fec511758d9e05419aac517f2
Jaewook Ahn <dev.jaewook@gmail.com>
2024-04-22 16:32:57 +0900

    add(template): baekjoon C++ code template

---
+++ templates/baekjoon.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+#define FASTIO cin.tie(0); cout.tie(0); ios::sync_with_stdio(0);
+#define ll long long int
+#define pq priority_queue
+#define INF int(2e9)
+#define LINF ll(1e18)
+
+int main() {
+    FASTIO;
+
+    return 0;
+}
