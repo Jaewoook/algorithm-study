b16f7f08234f5c46952ba3fa3bbac9528f5cc755
Jaewook Ahn <dev.jaewook@gmail.com>
2025-01-04 17:20:14 +0900

    template(leetcode): add c++ template

---
+++ templates/leetcode.cpp
@@ -0,0 +1,33 @@
+/**
+ * number:
+ * name:
+ * link: https://leetcode.com/problems/
+ */
+
+#include <iostream>
+
+using namespace std;
+// #define DEBUG
+
+class Solution
+{
+public:
+    uint32_t solution()
+    {
+        return 0;
+    }
+};
+
+#ifdef DEBUG
+#define FASTIO std::cin.tie(0); std::cout.tie(0); std::ios::sync_with_stdio(0);
+
+int main()
+{
+    FASTIO;
+    Solution s;
+
+    std::cout << s.solution() << std::endl;
+    return 0;
+}
+
+#endif
